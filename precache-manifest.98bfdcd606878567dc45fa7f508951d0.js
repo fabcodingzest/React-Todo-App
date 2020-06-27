f1ec969eff79ac4d7fe9a4cd5a89ea4d10aecb8c
fabcodingzest <fabrizvi786@gmail.com>
2020-06-27 22:09:53 +0530

    Updates

---
+++ precache-manifest.98bfdcd606878567dc45fa7f508951d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbb702c602a4256d6351093dd39679d",
+    "url": "/fabcodingzest/React-Todo-App/index.html"
+  },
+  {
+    "revision": "d835f6b82c384d908f05",
+    "url": "/fabcodingzest/React-Todo-App/static/css/main.e31aa496.chunk.css"
+  },
+  {
+    "revision": "caa77dd2d3c2818d42a6",
+    "url": "/fabcodingzest/React-Todo-App/static/js/2.78a02bfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fabcodingzest/React-Todo-App/static/js/2.78a02bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d835f6b82c384d908f05",
+    "url": "/fabcodingzest/React-Todo-App/static/js/main.d4b6cc15.chunk.js"
+  },
+  {
+    "revision": "88e2afe122fb29dd0daa",
+    "url": "/fabcodingzest/React-Todo-App/static/js/runtime-main.94955675.js"
+  }
+]);
